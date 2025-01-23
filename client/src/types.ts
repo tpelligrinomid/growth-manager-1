095505d964dac48ddb4ae67c1511dcee17ebd5d8
tpelligrinomid <tristan.pelligrino@marketersindemand.com>
2025-01-23 12:50:05 -0500

    added some stuff

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,3 +1,15 @@
+export enum EngagementType {
+  STRATEGIC = 'STRATEGIC',
+  TACTICAL = 'TACTICAL'
+}
+
+export enum Priority {
+  TIER_1 = 'TIER_1',
+  TIER_2 = 'TIER_2',
+  TIER_3 = 'TIER_3',
+  TIER_4 = 'TIER_4'
+}
+
 export enum Service {
   ABM = 'ABM',
   PAID_MEDIA = 'PAID_MEDIA',
