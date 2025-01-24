02474c4f95a670e8c09d0a01e158841b1ca439e7
tpelligrinomid <tristan.pelligrino@marketersindemand.com>
2025-01-24 12:12:35 -0500

    fix fetch

--- client/src/components/AccountModal.tsx
+++ client/src/components/AccountModal.tsx
@@ -93,9 +93,9 @@ const AccountModal: React.FC<Props> = ({ account, isOpen, onClose, onEdit, onUpd
         contractRenewalEnd: data.clientData?.[0]?.contract_renewal_end ? 
           new Date(data.clientData[0].contract_renewal_end.replace('/', '-')) : currentAccount.contractRenewalEnd,
         pointsPurchased: data.points?.[0]?.points_purchased ? 
-          Number(data.points[0].points_purchased.replace(/,/g, '')) : currentAccount.pointsPurchased,
+          Number(String(data.points[0].points_purchased).replace(/,/g, '')) : currentAccount.pointsPurchased,
         pointsDelivered: data.points?.[0]?.points_delivered ? 
-          Number(data.points[0].points_delivered.replace(/,/g, '')) : currentAccount.pointsDelivered,
+          Number(String(data.points[0].points_delivered).replace(/,/g, '')) : currentAccount.pointsDelivered,
         recurringPointsAllotment: data.clientData?.[0]?.recurring_points_allotment ? 
           Number(data.clientData[0].recurring_points_allotment.replace(/,/g, '')) : currentAccount.recurringPointsAllotment,
         mrr: data.clientData?.[0]?.mrr ? 
