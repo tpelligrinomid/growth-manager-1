4a802cffa13cc7127b1527940e50b8d8b1c3dd58
tpelligrinomid <tristan.pelligrino@marketersindemand.com>
2025-01-26 18:58:18 -0500

    stuff

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getUsers } from '../controllers/userController';
-import { authenticateToken } from '../middleware/auth';
+import { getUsers, updateRole } from '../controllers/userController';
+import { authenticateToken, requireRole } from '../middleware/auth';
+import { Role } from '@prisma/client';
 
 const router = Router();
 
 router.use(authenticateToken);
 router.get('/', getUsers);
+router.put('/:id/role', requireRole([Role.ADMINISTRATOR]), updateRole);
 
 export default router; 
