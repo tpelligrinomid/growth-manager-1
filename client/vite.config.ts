ccdab51afb19b5b9e6e6f5bf220504f3d27be791
tpelligrinomid <tristan.pelligrino@marketersindemand.com>
2025-01-26 11:12:32 -0500

    flow

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,18 +11,11 @@ export default defineConfig({
         target: 'http://localhost:3002',
         changeOrigin: true,
         secure: false,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response:', proxyRes.statusCode, req.url);
-          });
-        },
       },
     },
-  }
+  },
+  build: {
+    outDir: 'dist',
+  },
+  base: '/',
 })
